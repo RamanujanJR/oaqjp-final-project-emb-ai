5efa3a9c3f242dcd228799d701d56ecc5c45f035
Phuc <phucleduc2005@gmail.com>
2026-06-26 16:07:34 +0700

    Commit 2

--- test_emotion_detection.py
+++ test_emotion_detection.py
@@ -3,24 +3,24 @@ from EmotionDetection.emotion_detection import emotion_detector
 
 class TestEmotionDetector(unittest.TestCase):
     def test_emotion_detector(self):
-        # 1. Kiểm tra cảm xúc Vui vẻ (Joy)
+        # 1. Joy
         result_1 = emotion_detector("I am glad this happened")
         self.assertEqual(result_1['dominant_emotion'], 'joy')
         
-        # 2. Kiểm tra cảm xúc Giận dữ (Anger)
+        # 2. Anger
         result_2 = emotion_detector("I am really mad about this")
         self.assertEqual(result_2['dominant_emotion'], 'anger')
         
-        # 3. Kiểm tra cảm xúc Ghê tởm (Disgust)
-        result_3 = emotion_detector("I am really disgusted with the movie")
+        # 3. Disgust
+        result_3 = emotion_detector("I feel disgusted just hearing about this")
         self.assertEqual(result_3['dominant_emotion'], 'disgust')
         
-        # 4. Kiểm tra cảm xúc Sợ hãi (Fear)
-        result_4 = emotion_detector("I am shocked with the news")
+        # 4. Fear
+        result_4 = emotion_detector("I am really afraid that this will happen")
         self.assertEqual(result_4['dominant_emotion'], 'fear')
         
-        # 5. Kiểm tra cảm xúc Buồn bã (Sadness)
-        result_5 = emotion_detector("I think I am really depressed")
+        # 5. Sadness
+        result_5 = emotion_detector("I am so sad about this")
         self.assertEqual(result_5['dominant_emotion'], 'sadness')
 
 if __name__ == '__main__':
